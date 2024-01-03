7388619dfd0d800feb228793466ea3a8c4ea5f62
Glominictus <102306035+Glominictus@users.noreply.github.com>
2024-01-03 09:52:24 +0100

    Add files via upload

---
+++ Acceso/praza.php
@@ -0,0 +1,29 @@
+<?php 
+
+//llamando a los campos//
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$telefono = $_POST['telefono'];
+$materia = $_POST['materia'];
+$mensaje = $_POST['mensaje'];
+
+//Datos para el correo//
+$destinatario = "cmus.profesional.obarco@edu.xunta.gal";
+$asunto = "Contacto desde nuestra web";
+
+$carta = "De: $nombre \n";
+$carta .= "Correo: $email \n";
+$carta .= "Teléfono: $telefono \n";
+$carta .= "Materia: $materia \n";
+$carta .= "Mensaje: $mensaje";
+$headers = "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+$headers .= "From: Secretria del Conservatorio secretaria@obarco.com\r\n";
+
+
+//Enviando mensaje//
+mail($destinatario, $asunto, $carta, $headers);
+header("Location: envio-correcto.html");
+exit;
+
+?>
