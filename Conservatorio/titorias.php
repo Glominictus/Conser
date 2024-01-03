af758449495df97653772afa05c6bd87f90c2f1e
Glominictus <102306035+Glominictus@users.noreply.github.com>
2024-01-03 09:55:16 +0100

    Add files via upload

---
+++ Conservatorio/titorias.php
@@ -0,0 +1,29 @@
+<?php 
+
+//llamando a los campos//
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$profesor =$_POST['profesor'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+//Datos para el correo//
+$destinatario = "cmus.profesional.obarco@edu.xunta.gal";
+$asunto = "Contacto desde nuestra web";
+
+$carta = "De: $nombre \n";
+$carta .= "Correo: $email \n";
+$carta .= "Teléfono: $telefono \n";
+$carta .= "Profesor: $profesor \n";
+$carta .= "Mensaje: $mensaje";
+$headers = "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+$headers .= "From: Secretria del Conservatorio secretaria@obarco.com\r\n";
+
+
+//Enviando mensaje//
+mail($destinatario, $asunto, $carta, $headers);
+header("Location: envio-correcto.html");    
+exit;
+
+?>
